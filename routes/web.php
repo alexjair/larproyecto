6c12a4970ee418350a7c1655aeeae343963d940c
Alexander Jair Rojas Paria <alexjair@gmail.com>
2023-04-03 00:25:37 -0500

    Se agrego nuevas vistas y router

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('blog', function () {
+    //consulta a la base de datos
+    $posts = [
+        ['id' => 1, 'title' => 'PHP',       'slug' => 'php'],
+        ['id' => 3, 'title' => 'Laravel',   'slug' => 'laravel']
+    ];
+
+    return view('blog', ['posts' => $posts]);
+});
+
+Route::get('blog/{slug}', function ($slug) {
+    //consulta a la base de datos
+    $posts = $slug;
+
+     return view('post', ['post' => $post]);
+});
+ 
